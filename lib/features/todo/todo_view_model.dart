f69bf43f188eaa119688b8a76b8ab977d360d94e
« ghisswill» <kkghislain@hotmail.fr>
2026-01-18 09:41:57 +0100

    feature: persitence des données en local

--- lib/features/todo/todo_view_model.dart
+++ lib/features/todo/todo_view_model.dart
@@ -1,10 +1,41 @@
+import 'package:dimbalima_app/features/todo/todo.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TodoViewModel extends ChangeNotifier {
-  int counter = 0;
+  static const _stockageKey = 'todo';
+  final List<Todo> _todos = [];
 
-  void increment() {
-    counter ++;
+  List<Todo> get todos => _todos;
+
+  TodoViewModel() {
+    _loadTodos();
+  }
+
+  void addTodo(String title) {
+    if(title.isEmpty) return;
+    _todos.add(Todo(title));
+    _saveTodo();
+    notifyListeners();
+  }
+
+  void removeTodo(int index) {
+    _todos.removeAt(index);
+    _saveTodo();
+    notifyListeners();
+  }
+
+  Future<void>_saveTodo() async {
+    final prefs = await SharedPreferences.getInstance();
+    final titles = _todos.map((e)=> e.title).toList();
+    await prefs.setStringList(_stockageKey, titles);
+  }
+
+  Future<void>_loadTodos() async {
+    final perfs = await SharedPreferences.getInstance();
+    final titles = perfs.getStringList(_stockageKey) ?? [];
+    _todos.clear();
+    _todos.addAll(titles.map((e)=>Todo(e)));
     notifyListeners();
   }
 }
